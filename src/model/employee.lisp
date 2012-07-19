4c6cb08bd81d1934c3fbd60db86b096b4add4226
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-07-19 22:34:25 +0300

    Fixed #1

--- src/model/employee.lisp
+++ src/model/employee.lisp
@@ -8,9 +8,3 @@
 
 ;;; Table View
 (defview employee-table-view (:type table :inherit-from 'person-table-view))
-
-;;; Form View
-(defview employee-form-view (:type form :inherit-from 'person-form-view
-				   :caption "Person")
-  (FoUU-roll :present-as (radio :choices '(:kontaktperson :projektledare :verksamhetschef))
-	    :parse-as keyword))
--- src/widgets/employees-grid.lisp
+++ src/widgets/employees-grid.lisp
@@ -32,11 +32,62 @@
 (defmethod dataedit-update-operations :around ((obj employees-grid) &key
 				   (delete-fn #'dataedit-delete-items-flow)
 				   (add-fn #'dataedit-add-items-flow))
-  "Should be used to update operations the widget supports.
-
-'delete-fn' - a function to be called when delete action is invoked.
-'add-fn' - a function to be called when add action is invoked."
   (call-next-method)
   (pushnew (cons 'add-person-to-project #'employees-grid-add-persons-to-projects-flow)
            (dataseq-item-ops obj)
            :key #'car))
+
+(let ((session-key  'employee-form-view-showed-repeat-message))
+  (defmethod dataedit-create-new-item-widget :around ((grid employees-grid))
+    (delete-webapp-session-value 'employee-form-view-showed-repeat-message)
+    (call-next-method))
+
+  ;;; Form View
+  (defview employee-form-view (:type form :inherit-from 'person-form-view
+                               :satisfies (lambda (&rest args)
+                                            (let ((employees-with-same-name-and-last-name 
+                                                    (find-by-values 'employee 
+                                                                    :förnamn (getf args :förnamn) 
+                                                                    :efternamn (getf args :efternamn))))
+                                              (if (and (not (webapp-session-value session-key)) employees-with-same-name-and-last-name)
+                                                (progn 
+                                                  (setf (webapp-session-value session-key) t)
+                                                  (values nil "There is already employee with same name. Press submit button again to force adding."))
+                                                (progn 
+                                                  (delete-webapp-session-value session-key)
+                                                  t))))
+                               :caption "Person")
+           (FoUU-roll :present-as (radio :choices '(:kontaktperson :projektledare :verksamhetschef))
+                         :parse-as keyword)))
+
+; This update needed to fix bug
+; source: weblocks-20120305-git/src/views/types/presentations/radio.lisp
+;
+(defmethod render-view-field ((field form-view-field) (view form-view)
+			      widget (presentation radio-presentation) value obj
+			      &rest args &key validation-errors field-info &allow-other-keys)
+  (let* ((attribute-slot-name (if field-info
+                                (attributize-view-field-name field-info)
+                                (attributize-name (view-field-slot-name field))))
+	 (validation-error (assoc attribute-slot-name (remove-if (lambda (item) (null (car item))) validation-errors)
+				  :test #'string-equal
+				  :key #'view-field-slot-name))
+	 (field-class (concatenate 'string attribute-slot-name
+				   (when validation-error " item-not-validated"))))
+    (with-html
+      (:li :class field-class
+	   (:span :class "label"
+		  (:span :class "slot-name"
+			 (:span :class "extra"
+				(str (view-field-label field)) ":&nbsp;"
+                                (when (form-view-field-required-p field)
+                                  (htm (:em :class "required-slot" "(required)&nbsp;"))))))
+	   (apply #'render-view-field-value
+		  value presentation
+		  field view widget obj
+		  args)
+	   (when validation-error
+	     (htm (:p :class "validation-error"
+		      (:em
+		       (:span :class "validation-error-heading" "Error:&nbsp;")
+		       (str (format nil "~A" (cdr validation-error)))))))))))
