cf858d413b462a07a410204964f0f4c04b81864d
Olexiy Zamkoviy <olexiy.z@gmail.com>
2012-07-22 17:09:17 +0300

    Added new project loader

--- rundemo.lisp
+++
@@ -1,4 +0,0 @@
-(push #p"/usr/lib/sbcl/site/cl-weblocks/" asdf:*central-registry*)
-(push #p"/home/mikael/Common-Lisp/own-projects/primarvarden/" asdf:*central-registry*)
-(asdf:operate 'asdf:load-op :primarvarden)
-(primarvarden:start-primarvarden)
