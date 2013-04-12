f5fda6775ab84c1721dbca53ee42df5c4a196f76
Mikael Andersson <mikael@gnutiken.se>
2013-04-12 15:33:04 +0200

    Added contact details in footer of the page body.

--- webapp/src/snippets.lisp
+++ webapp/src/snippets.lisp
@@ -18,8 +18,14 @@
 		(str (concatenate 'string (server-type) " " (server-version)))
 		" (" (str (concatenate 'string (lisp-implementation-type) " "
 				      (lisp-implementation-version))) ")")|#
+	    (:p :id "description" :style "color:white;"
+		"Based on the Common Lisp web framework "
+		(:a :href "http://weblocks.viridian-project.de/" "Weblocks") "and the demo app weblocks-demo.")
 	    (:p :id "contact-info" :style "color:white;"
-		"Based on the Common Lisp web framework " (:a :href "http://weblocks.viridian-project.de/" "Weblocks") ".") (:br)
+		"Developed by " (:a :href "mailto:mikael@fripost.org" "Mikael Andersson") "and "
+		(:a :href "mailto:olexiy.z@gmail.com" "Olexiy Zamkoviy"))
+	    (:br)
+	    
 	    ;;(:img :src "/pub/images/footer/valid-xhtml11.png" :alt "This site has valid XHTML 1.1.")
 	    ;;(:img :src "/pub/images/footer/valid-css.png" :alt "This site has valid CSS.")
 	    )))
